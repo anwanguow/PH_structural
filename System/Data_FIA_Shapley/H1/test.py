fd95b33e596a29af5b130965fe297182e462ed8f
anwanguow <143639727+anwanguow@users.noreply.github.com>
2024-11-17 16:46:49 +0000

    Update 10
    Update 10

---
+++ System/Data_FIA_Shapley/H1/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+frames0 = [100] + list(range(125, 925, 25))
---
+++ System/Data_FIA_Shapley/H2/code/PH_3.py
@@ -0,0 +1,24 @@
+import numpy as np
+import os
+import pickle
+from PH import PH_barcode_h2
+
+set_dirs = ["../../data/distance_matrix/set_1", "../../data/distance_matrix/set_2"]
+output_dirs = ["../PH/barcode/set_1", "../PH/barcode/set_2"]
+frames = [400] + list(range(425, 500, 25))
+
+for set_index, set_dir in enumerate(set_dirs):
+    for traj_index in range(10):
+        traj_dir = os.path.join(set_dir, f"D_{traj_index}")
+        output_traj_dir = os.path.join(output_dirs[set_index], f"D_{traj_index}")
+        os.makedirs(output_traj_dir, exist_ok=True)
+        
+        for frame in frames:
+            distance_matrix_path = os.path.join(traj_dir, f"D_{frame}.npy")
+            D = np.load(distance_matrix_path)
+
+            barcode = PH_barcode_h2(D)
+            
+            output_path = os.path.join(output_traj_dir, f"D_{frame}.pkl")
+            with open(output_path, 'wb') as f:
+                pickle.dump(barcode, f)
